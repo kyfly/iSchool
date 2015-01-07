60ec499aaaa57a0f3799fbf9e803bd2b7b302400
wrq <348928400@qq.com>
2015-01-07 16:54:51 +0800

    获取parent和teacher对应的接口。 分别为api/teachers/getAllTeachers   api/parent/getAllParents

--- common/models/teacher.js
+++ common/models/teacher.js
@@ -1,3 +1,31 @@
 module.exports = function(Teacher) {
+    Teacher.getAllTeachers = function(id, cb){
+      var myuser = Teacher.app.models.myuser;
+      Teacher.find({
+          include: {
+            relation: 'belongToMyUser',
+            scope: {
+              fields: ['name']
+            }
+          }
+        },
+        function(err, teachers){
+          console.log(teachers);
+          var result = [];
+          for(var key in teachers){
+            result[key] = {};
+            result[key].teacher_id = teachers[key].id;
+            result[key].name = teachers[key].__data.belongToMyUser.name;
+          }
+          cb(null, result);
+        });
+    };
 
+    Teacher.remoteMethod(
+      'getAllTeachers',
+      {
+        accepts: {arg: 'id', type:'number'},
+        returns: {arg: 'teachers', type: 'string'}
+      }
+    );
 };
