2ed77febf8e07bc60bd2fdac39fab72afa809bf4
evilucifero <evilucifero@163.com>
2015-01-07 08:03:25 +0800

    完成登录功能

---
+++ client/js/login.js
@@ -0,0 +1,21 @@
+var login = angular.module("login", ['ngCookies']);
+
+login.controller('signin',['$scope','$http','$cookieStore', function ($scope,$http,$cookieStore) {
+	$scope.login = function() {
+		this.credentials = {
+			email:this.inputEmail,
+			password:this.inputPassword
+		}
+		$http.post('/api/myusers/login',this.credentials)
+		.success(function(data){
+			$cookieStore.put('access_token',data.id);
+			window.location = '/index.html';
+		})
+		.error(function(){
+			alert("登录失败");
+		});
+	}
+	$scope.redirectToSignup = function () {
+		window.location = '/signup.html';
+	}
+]);
