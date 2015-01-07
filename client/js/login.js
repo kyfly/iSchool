a7a402a8ca1ffa1cd8fcb9b8485c1a35c4e3a7fb
evilucifero <evilucifero@163.com>
2015-01-07 12:14:07 +0800

    完成登录时候的cookie使用

--- client/js/login.js
+++ client/js/login.js
@@ -9,12 +9,34 @@ login.controller('loginCtrl',['$scope','$http','$cookieStore', function ($scope,
 		$http.post('/api/myusers/login',this.credentials)
 		.success(function(data){
 			$cookieStore.put('accessToken',data.id);
-			$cookieStore.put('userId',data.userId)
-			window.location = '/index.html';
+			$cookieStore.put('userId',data.userId);
+			$http.get('/api/myusers/'+data.userId+'/?access_token='+data.id)
+			.success(function(extradata){
+				$cookieStore.put('userType',extradata.type);
+				if (extradata.type === 0){
+					$http.get('/api/teachers/?access_token='+data.id+'&filter=%7B%22where%22%3A%7B%22user_id%22%3A%22'+data.userId+'%22%7D%7D')
+					.success(function(detaildata){
+						$cookieStore.put('detailId',detaildata[0].id);
+					});
+				}
+				else if (extradata.type === 1){
+					$http.get('/api/parents/?access_token='+data.id+'&filter=%7B%22where%22%3A%7B%22user_id%22%3A%22'+data.userId+'%22%7D%7D')
+					.success(function(detaildata){
+						$cookieStore.put('detailId',detaildata[0].id);
+					});
+				}
+				else if (extradata.type === 2){
+					$http.get('/api/students/?access_token='+data.id+'&filter=%7B%22where%22%3A%7B%22user_id%22%3A%22'+data.userId+'%22%7D%7D')
+					.success(function(detaildata){
+						$cookieStore.put('detailId',detaildata[0].id);
+					});
+				}
+				window.location = '/index.html';
+			})
+			.error(function(){
+				alert("登录失败");
+			});
 		})
-		.error(function(){
-			alert("登录失败");
-		});
 	}
 	$scope.redirectToSignup = function () {
 		window.location = '/signup.html';
