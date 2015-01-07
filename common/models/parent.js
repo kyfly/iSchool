3486ebd8f2e12343d4e316f358fea75b2247decd
yzf <youzhenfei@126.com>
2015-01-07 16:43:24 +0800

    增加获取家长发送给老师的全部消息的接口

--- common/models/parent.js
+++ common/models/parent.js
@@ -1,3 +1,21 @@
 module.exports = function(Parent) {
+    Parent.messages = function(cb) {
+        Parent.app.models.Parent.find({fields: {_messages: true}}, function(err, result){
+            var msgList = [];
+            result.forEach(function(msgs){
+                msgs.__data._messages.forEach(function(msg){
+                    msgList.push(msg);
+                })
+            });
+            cb(null, msgList);
+        });
 
+    };
+    Parent.remoteMethod(
+        'messages',
+        {
+            http: {verb:'get'},
+            returns: {arg: 'messages', type: 'object'}
+        }
+    );
 };
